e97360f86539d308a5b117b0207352d14fbd9d0f
Kalilovers <30160766+kalilovers@users.noreply.github.com>
2025-01-28 11:45:14 +0330

    Update lightweight.sh

--- lightweight.sh
+++ lightweight.sh
@@ -6,5 +6,5 @@ chmod +x /etc/logo2.sh
 if [ -f "light_script.py" ]; then
     rm light_script.py
 fi
-wget https://github.com/kalilovers/LightKnightBBR/releases/download/download/light_script.py
+wget https://github.com/kalilovers/6TO4-all/releases/download/download/light_script.py
 python3 light_script.py
